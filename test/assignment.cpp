90c9feb5f9db83a98f2b1bad71790f6842786a44
Chylynsky <borchy97@gmail.com>
2021-10-10 17:47:04 +0200

    [test] Add assignment tests for weak_rc_ptr.

--- test/assignment.cpp
+++ test/assignment.cpp
@@ -13,7 +13,8 @@
 
 TEST_CASE("rc_ptr, nullptr assignment", "[assignment]")
 {
-    memory::rc_ptr<int> ptr = nullptr;
+    memory::rc_ptr<int> ptr;
+    ptr = nullptr;
     REQUIRE(ptr.get() == nullptr);
     REQUIRE(ptr.use_count() == 0);
     REQUIRE(!ptr.unique());
@@ -23,7 +24,8 @@ TEST_CASE("rc_ptr, copy assignment", "[assignment]")
 {
     auto raw = new int{ 0 };
     memory::rc_ptr<int> first{ raw };
-    memory::rc_ptr<int> second = first;
+    memory::rc_ptr<int> second;
+    second = first;
     REQUIRE(first.get() == raw);
     REQUIRE(first.use_count() == 2);
     REQUIRE(!first.unique());
@@ -35,7 +37,8 @@ TEST_CASE("rc_ptr, copy assignment", "[assignment]")
 TEST_CASE("rc_ptr, copy assignment when nullptr", "[assignment]")
 {
     memory::rc_ptr<int> first{ nullptr };
-    memory::rc_ptr<int> second = first;
+    memory::rc_ptr<int> second;
+    second = first;
     REQUIRE(first.get() == nullptr);
     REQUIRE(first.use_count() == 0);
     REQUIRE(!first.unique());
@@ -48,7 +51,8 @@ TEST_CASE("rc_ptr, move assignment", "[assignment]")
 {
     auto raw = new int{ 0 };
     memory::rc_ptr<int> first{ raw };
-    memory::rc_ptr<int> second = std::move(first);
+    memory::rc_ptr<int> second;
+    second = std::move(first);
     REQUIRE(first.get() == nullptr);
     REQUIRE(first.use_count() == 0);
     REQUIRE(!first.unique());
@@ -60,7 +64,8 @@ TEST_CASE("rc_ptr, move assignment", "[assignment]")
 TEST_CASE("rc_ptr, move assignment when nullptr", "[assignment]")
 {
     memory::rc_ptr<int> first{ nullptr };
-    memory::rc_ptr<int> second = std::move(first);
+    memory::rc_ptr<int> second;
+    second = std::move(first);
     REQUIRE(first.get() == nullptr);
     REQUIRE(first.use_count() == 0);
     REQUIRE(!first.unique());
@@ -71,7 +76,8 @@ TEST_CASE("rc_ptr, move assignment when nullptr", "[assignment]")
 
 TEST_CASE("rc_ptr, nullptr assignment with deleter", "[assignment]")
 {
-    auto ptr = memory::rc_ptr<int, std::function<void(int*)>>{
+    memory::rc_ptr<int, std::function<void(int*)>> ptr;
+    ptr = memory::rc_ptr<int, std::function<void(int*)>>{
         nullptr,
         [](int* ptr) { delete ptr; },
     };
@@ -83,11 +89,90 @@ TEST_CASE("rc_ptr, nullptr assignment with deleter", "[assignment]")
 TEST_CASE("rc_ptr, pointer assignment with deleter", "[assignment]")
 {
     auto raw = new int{ 0 };
-    auto ptr = memory::rc_ptr<int, std::function<void(int*)>>{
+    memory::rc_ptr<int, std::function<void(int*)>> ptr;
+    ptr = memory::rc_ptr<int, std::function<void(int*)>>{
         raw,
         [](int* ptr) { delete ptr; },
     };
     REQUIRE(ptr.get() != nullptr);
     REQUIRE(ptr.use_count() == 1);
     REQUIRE(ptr.unique());
+}
+
+TEST_CASE("rc_ptr, assignment of weak_rc_ptr when valid", "[assignment]")
+{
+    auto raw = new int{ 0 };
+    memory::rc_ptr<int> first{ raw };
+    memory::weak_rc_ptr<int> second{ first };
+    memory::rc_ptr<int> third;
+    third = second;
+    REQUIRE(first.get() == raw);
+    REQUIRE(first.use_count() == 2);
+    REQUIRE(!first.unique());
+    REQUIRE(!second.expired());
+    REQUIRE(second.use_count() == 2);
+    REQUIRE(third.get() == raw);
+    REQUIRE(third.use_count() == 2);
+    REQUIRE(!third.unique());
+}
+
+TEST_CASE("rc_ptr, assignment of weak_rc_ptr when invalid", "[assignment]")
+{
+    memory::rc_ptr<int> first{ nullptr };
+    memory::weak_rc_ptr<int> second{ first };
+    memory::rc_ptr<int> third;
+    REQUIRE_THROWS_AS(std::invoke([&]() { third = second; }), memory::bad_weak_rc_ptr);
+}
+
+TEST_CASE("weak_rc_ptr, copy assignment when default constructed", "[assignment]")
+{
+    memory::weak_rc_ptr<int> first;
+    memory::weak_rc_ptr<int> second;
+    second = first;
+    REQUIRE(first.expired());
+    REQUIRE(first.use_count() == 0);
+    REQUIRE(second.expired());
+    REQUIRE(second.use_count() == 0);
+}
+
+TEST_CASE("weak_rc_ptr, copy assignment when valid", "[assignment]")
+{
+    memory::rc_ptr<int> first{ new int{ 0 } };
+    memory::weak_rc_ptr<int> second{ first };
+    memory::weak_rc_ptr<int> third;
+    third = second;
+    REQUIRE(!second.expired());
+    REQUIRE(second.use_count() == 1);
+    REQUIRE(!third.expired());
+    REQUIRE(third.use_count() == 1);
+}
+
+TEST_CASE("weak_rc_ptr, move assignment when valid", "[assignment]")
+{
+    memory::rc_ptr<int> first{ new int{ 0 } };
+    memory::weak_rc_ptr<int> second{ first };
+    memory::weak_rc_ptr<int> third;
+    third = std::move(second);
+    REQUIRE(second.expired());
+    REQUIRE(second.use_count() == 0);
+    REQUIRE(!third.expired());
+    REQUIRE(third.use_count() == 1);
+}
+
+TEST_CASE("weak_rc_ptr, assignment of valid rc_ptr", "[assignment]")
+{
+    memory::rc_ptr<int> first{ new int{ 0 } };
+    memory::weak_rc_ptr<int> second;
+    second = first;
+    REQUIRE(!second.expired());
+    REQUIRE(second.use_count() == 1);
+}
+
+TEST_CASE("weak_rc_ptr, assignment of invalid rc_ptr", "[assignment]")
+{
+    memory::rc_ptr<int> first{ nullptr };
+    memory::weak_rc_ptr<int> second;
+    second = first;
+    REQUIRE(second.expired());
+    REQUIRE(second.use_count() == 0);
 }
